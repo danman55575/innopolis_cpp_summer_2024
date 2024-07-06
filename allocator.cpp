b6658ffb711d7911bce2dbe8086ee2a9a43cf59f
danman55575 <100946635+danman55575@users.noreply.github.com>
2024-07-06 16:46:16 +0300

    Mistakes correct

--- allocator.cpp
+++ allocator.cpp
@@ -1,12 +1,31 @@
 #include <cstddef>
 #include <cstdlib>
 #include <iostream>
+#include <stdexcept>
 #include "allocator.hpp"
+
+class LackMemory {
+private:
+    std::string message_;
+public:
+    LackMemory(const std::string& message): message_(message) {}
+    std::string what() const {
+        return this->message_;
+    }
+};
+
+Allocator::Allocator() {
+    this->ptr_to_start = nullptr;
+}
 
 void Allocator::makeAllocator(size_t maxSize)
 {
     delete[] this->ptr_to_start;
-    this->ptr_to_start = new char[maxSize];
+    try {
+        this->ptr_to_start = new char[maxSize];
+    } catch(const std::bad_alloc& err) {
+        throw LackMemory("Error with memory allocation!");
+    }
     this->size_used = 0;
     this->max_size = maxSize;
 }
